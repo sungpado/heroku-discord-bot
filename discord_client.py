5497421e11a84ba59b6c8a99cf0874f3434b3671
sungpado <34066247+sungpado@users.noreply.github.com>
2018-02-16 23:24:42 +0900

    Update discord_client.py

--- discord_client.py
+++ discord_client.py
@@ -4,19 +4,19 @@ import os
 import time as t
 t2 = t.gmtime()
 if t2.tm_wday == 0:
-    w = '월'
-elif t2.tm_wday == 1:
-    w = '화'  
-elif t2.tm_wday == 2:
-    w = '수'
-elif t2.tm_wday == 3:
-    w = '목'
-elif t2.tm_wday == 4:
-    w = '금'
-elif t2.tm_wday == 5:
-    w = '토'
-elif t2.tm_wday == 6:
     w = '일'
+elif t2.tm_wday == 1:
+    w = '월'  
+elif t2.tm_wday == 2:
+    w = '화'
+elif t2.tm_wday == 3:
+    w = '수'
+elif t2.tm_wday == 4:
+    w = '목'
+elif t2.tm_wday == 5:
+    w = '금'
+elif t2.tm_wday == 6:
+    w = ''
 else :
     pass
 
